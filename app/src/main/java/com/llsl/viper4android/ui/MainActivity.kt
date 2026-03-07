4c3661c7e60b5418dfb617caf24e85838219b859
zhaoyang <zhaoyangliu10@gmail.com>
2026-03-08 00:00:25 +0800

    feat: Start service in main activity

--- app/src/main/java/com/llsl/viper4android/ui/MainActivity.kt
+++ app/src/main/java/com/llsl/viper4android/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.llsl.viper4android.service.ViperService
 import com.llsl.viper4android.ui.navigation.ViperNavigation
 import com.llsl.viper4android.ui.theme.ViperTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ViperService.startService(this)
         enableEdgeToEdge()
         setContent {
             ViperTheme {
